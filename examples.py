7f80ecb70ed7d6370d20c5360e7727d1da23c366
Steve <steveariasarias@hotmail.com>
2019-11-23 13:26:59 -0500

    Change examples

--- examples.py
+++ examples.py
@@ -1,16 +1,17 @@
-def printMessage():
+def imprimirMensaje():
 	print( "Hola Mundo" )
 
-def functionWithReturn( lado) :
+def calcularArea( lado) :
 	area = lado*lado
 	return area  
 	# print("Area",area)
 
-def functionWithoutReturn( a,b,c) : 
-	discriminante =   pow(b,2) - 4*a*c 
+def calcularDiscriminante( a,b,c) : 
+	discriminante =  pow(b,2) - 4*a*c 
 	print("La discriminante es ", discriminante)
 
 
-functionWithoutReturn(2,2,1)
+# calcularDiscriminante(2,2,1)
+
 
 # print("el resultado es :",resultado)
